80d8977f2ce6abd2b7f49d6ef7972d07d7d92316
Mariano Reingart <reingart@gmail.com>
2015-02-04 19:56:01 -0300

    Fix version number to comply with PEP 440 and pypi...

--- pysimplesoap/client.py
+++ pysimplesoap/client.py
@@ -15,7 +15,7 @@
 __author__ = "Mariano Reingart (reingart@gmail.com)"
 __copyright__ = "Copyright (C) 2008 Mariano Reingart"
 __license__ = "LGPL 3.0"
-__version__ = "1.08e"
+__version__ = "1.08.5"
 
 TIMEOUT = 60
 
